c8f51e4e70aa8d4a1e8bbd7ea02945d59a39a48b
Manhal-Baban <manhal.baban@gmail.com>
2025-11-11 10:26:58 +0200

    some card and form updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,15 @@
 import styles from "./Card.module.css";
 
-const Card = (props) => {
+const Card = ({ name, title, age, isFavourite, toggleFavourite, id }) => {
+  console.log("isFavourite", isFavourite);
+
   return (
     <div className={styles.card}>
-      <div className={styles.name}>{props.name}</div>
-      <div className={styles.title}>Title: {props.title}</div>
-      <div className={styles.salary}>Salary: {props.salary}</div>
-      <div className={styles.phone}>Phone: {props.phone}</div>
-      <div className={styles.email}>Email: {props.email}</div>
-      <div className={styles.animal}>Animal: {props.animal}</div>
+      <button onClick={() => toggleFavourite(id)}>Toggle Favourite</button>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.title}>Title: {title}</div>
+      <div className={styles.age}>Age: {age}</div>
+      <div className={styles.favourite}>{isFavourite && <span>❤️</span>}</div>
     </div>
   );
 };
